102de2f777e1066f37f889a9c3847d962d601782
Nguyen Van Nghi <vannghibg03@gmail.com>
2024-08-14 00:33:03 +0700

    Test branch NghiNV2003

--- src/main/java/org/pokemon/pokemonapi/api/controllers/PokemonController.java
+++ src/main/java/org/pokemon/pokemonapi/api/controllers/PokemonController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-// pokemon controller
+// pokemon controller test
 @RestController
 @RequestMapping("/api")
 public class PokemonController {
